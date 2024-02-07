749e99975492f6b67d83167cabd37adba9d9fbef
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-02-07 00:39:50 +0000

    flux: missing kubectl version

--- modules/flux/versions.tf
+++ modules/flux/versions.tf
@@ -6,10 +6,15 @@ terraform {
       source  = "fluxcd/flux"
       version = "~> 1.2"
     }
+
     github = {
       source  = "integrations/github"
       version = "~> 5.18"
     }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
   }
 }
-
