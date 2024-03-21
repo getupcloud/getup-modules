b9342264616509d779c8630afdcb7e985b6dc033
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-03-21 19:11:14 +0000

    Auto-generated examples v0.13.0

--- examples/ecr-credentials-sync/main-ecr-credentials-sync.tf
+++ examples/ecr-credentials-sync/main-ecr-credentials-sync.tf
@@ -1,7 +1,7 @@
 # File auto-generated from ./bin/vars2tf
 
 module "ecr-credentials-sync" {
-  source = "github.com/getupcloud/terraform-modules//modules/ecr-credentials-sync?ref=v0.12.2"
+  source = "github.com/getupcloud/terraform-modules//modules/ecr-credentials-sync?ref=v0.13.0"
 
   ecr_credentials_sync_cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
   ecr_credentials_sync_tags                    = var.ecr_credentials_sync_tags
