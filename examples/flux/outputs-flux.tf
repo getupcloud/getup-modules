311c90f9f8639dbba32c82bec89093cd7babc6c6
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-05-14 21:22:34 +0100

    fix karpenter version; sensitive output

--- examples/flux/outputs-flux.tf
+++ examples/flux/outputs-flux.tf
@@ -3,4 +3,5 @@
 output "private_key_pem" {
   description = "TLS Private keys for flux git repository."
   value       = module.flux.private_key_pem
+  sensitive   = True
 }
--- modules/flux/outputs.tf
+++ modules/flux/outputs.tf
@@ -4,4 +4,5 @@
 output "private_key_pem" {
   description = "TLS Private keys for flux git repository."
   value       = tls_private_key.flux.private_key_pem
+  sensitive   = true
 }
