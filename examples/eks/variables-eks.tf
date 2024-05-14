d3da266c803f92734b601b674d10a9386b4fb7d5
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-05-14 18:12:33 +0100

    update eks default version

--- examples/eks/variables-eks.tf
+++ examples/eks/variables-eks.tf
@@ -11,7 +11,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes controlplane version."
   type        = string
-  default     = "1.28"
+  default     = "1.29"
 }
 
 variable "aws_region" {
