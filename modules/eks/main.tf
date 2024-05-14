948d87ba38912f9a87b63b973b8d8716ee13b427
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-05-14 20:01:55 +0100

    fmt

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -256,7 +256,7 @@ module "karpenter" {
   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
   version = "~> 20.10.0"
 
-  cluster_name = module.eks.cluster_name
+  cluster_name                    = module.eks.cluster_name
   enable_pod_identity             = true
   create_pod_identity_association = true
 
