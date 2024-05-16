4ff267c37fa3f6d62aba2a48a2c13ac8db7fbc5d
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-05-16 01:43:55 +0100

    fmt

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -25,8 +25,8 @@ resource "aws_iam_service_linked_role" "spot" {
   aws_service_name = "spot.amazonaws.com"
 
   lifecycle {
-    replace_triggered_by = [ data.aws_caller_identity.current.account_id ]
-    ignore_changes = [ aws_service_name ]
+    replace_triggered_by = [data.aws_caller_identity.current.account_id]
+    ignore_changes       = [aws_service_name]
   }
 }
 
