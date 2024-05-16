4f4971077540d6fd5f9bd3f1aeb04fb8297c62b3
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-05-16 18:16:08 +0100

    Auto-generated examples v0.15.16

--- examples/eks/main-eks.tf
+++ examples/eks/main-eks.tf
@@ -1,7 +1,7 @@
 # File auto-generated from ./bin/vars2tf
 
 module "eks" {
-  source = "github.com/getupcloud/terraform-modules//modules/eks?ref=v0.15.15"
+  source = "github.com/getupcloud/terraform-modules//modules/eks?ref=v0.15.16"
 
   cluster_name                           = var.cluster_name
   kubernetes_version                     = var.kubernetes_version
