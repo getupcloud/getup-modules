fedc6bd9f7bc65629c1de3ce36ded85ad5eea72a
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-05-22 21:38:18 +0100

    Auto-generated examples v0.16.4

--- examples/aws-external-secrets-operator/main-aws-external-secrets-operator.tf
+++ examples/aws-external-secrets-operator/main-aws-external-secrets-operator.tf
@@ -1,7 +1,7 @@
 # File auto-generated from ./bin/vars2tf
 
 module "aws-external-secrets-operator" {
-  source = "github.com/getupcloud/terraform-modules//modules/aws-external-secrets-operator?ref=v0.16.3"
+  source = "github.com/getupcloud/terraform-modules//modules/aws-external-secrets-operator?ref=v0.16.4"
 
   aws_eso_cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
   aws_eso_namespace               = var.aws_eso_namespace
--- examples/flux/outputs-flux.tf
+++ examples/flux/outputs-flux.tf
@@ -5,8 +5,3 @@ output "private_key_pem" {
   value       = module.flux.private_key_pem
   sensitive   = true
 }
-
-output "flux_overlay" {
-  description = "Map of variables to inject into overlay files."
-  value       = module.flux.flux_overlay
-}
