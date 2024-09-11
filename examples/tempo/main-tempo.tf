4e1c364b096cb5ee5dd8145b24743aef2e19b0a1
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-09-11 23:33:54 +0100

    Update tempo example

--- examples/tempo/main-tempo.tf
+++ examples/tempo/main-tempo.tf
@@ -3,7 +3,7 @@
 module "tempo" {
   source = "github.com/getupcloud/terraform-modules//modules/tempo?ref=v0.17.20"
 
-  tempo_cluster_oidc_issuer_url = var.tempo_cluster_oidc_issuer_url
+  tempo_cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
   tempo_tags                    = var.tempo_tags
   tempo_retention_days          = var.tempo_retention_days
 }
