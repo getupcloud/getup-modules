740291f01ae7f948522665d1ce425eebde62083a
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2025-01-17 18:23:50 +0000

    Fix velero oidc url vars

--- examples/velero/main-velero.tf
+++ examples/velero/main-velero.tf
@@ -3,7 +3,7 @@
 module "velero" {
   source = "github.com/getupcloud/terraform-modules//modules/velero?ref=v0.21.0"
 
-  velero_cluster_oidc_issuer_url = var.velero_cluster_oidc_issuer_url
+  velero_cluster_oidc_issuer_url = module.velero.cluster_oidc_issuer_url
   velero_tags                    = var.velero_tags
   velero_retention_days          = var.velero_retention_days
   velero_retention_prefixes      = var.velero_retention_prefixes
