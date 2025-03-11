608eac7f33ba755c079c3a030b592deaa6539eda
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2025-03-11 16:30:29 +0000

    Auto-generated examples v0.23.2

--- examples/eks/variables-eks.tf
+++ examples/eks/variables-eks.tf
@@ -50,25 +50,6 @@ variable "tags" {
   }
 }
 
-variable "overlay" {
-  description = "Map of variables to inject into overlay files."
-  type        = map(string)
-  default = {
-    certmanager_acme_email : ""
-    cronitor_ping_url : ""
-    ecr_credential_sync_region : ""
-    msteams_channel_url : ""
-    opencost_spot_datafeed_bucket_name : ""
-    opencost_spot_datafeed_bucket_prefix : ""
-    opencost_storage_class_name : ""
-    opsgenie_integration_api_key : ""
-    pagerduty_service_key : ""
-    slack_api_url : ""
-    slack_channel : ""
-    teleport_auth_token : ""
-  }
-}
-
 #### Encryption ####
 ####################
 
