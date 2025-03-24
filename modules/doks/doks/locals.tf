1ddf8f9d94403b249d724abf5203fe4387095e48
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2025-03-24 18:21:23 +0000

    doks: fix tags

--- modules/doks/doks/locals.tf
+++ modules/doks/doks/locals.tf
@@ -1,6 +1,6 @@
 locals {
   vpc_create = var.vpc_uuid == "" && var.vpc_name != ""
   vpc_uuid   = local.vpc_create ? digitalocean_vpc.vpc[0].id : var.vpc_uuid
-  tags       = [for k, v in var.tags : "${k}=${v}"]
+  tags       = [for k, v in var.tags : "${k}:${v}"]
 }
 
