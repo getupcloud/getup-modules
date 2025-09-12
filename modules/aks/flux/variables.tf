c9d87d5d6380b1e93c4d39d10c3167e5bb8bb649
Gustavo Carneiro <gustavo.carneiro@getupcloud.com>
2025-09-12 10:27:33 -0300

    feat: aks flux module

---
+++ modules/aks/flux/variables.tf
@@ -0,0 +1,43 @@
+#### Flux #####
+###############
+
+variable "flux_aks_region" {
+  description = "AKS Region."
+  type        = string
+}
+
+variable "flux_cluster_name" {
+  description = "AKS cluster name."
+  type        = string
+}
+
+variable "flux_github_token" {
+  description = "GitHub OAuth / Personal Access Token. Can also by provided via the GITHUB_TOKEN environment variable."
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "flux_github_org" {
+  description = "GitHub organization or individual user account to manage."
+  type        = string
+  default     = ""
+}
+
+variable "flux_github_repository" {
+  description = "Name of the GitHub repository."
+  type        = string
+  default     = ""
+}
+
+variable "flux_path" {
+  description = "Path relative to the repository root, when specified the cluster sync will be scoped to this path."
+  type        = string
+  default     = "cluster"
+}
+
+variable "flux_version" {
+  description = "Flux version."
+  type        = string
+  default     = "v2.3.0"
+}
---
+++ modules/aks/flux/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.12"
+
+  required_providers {
+    flux = {
+      source  = "fluxcd/flux"
+      version = "~> 1.2"
+    }
+
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.18"
+    }
+
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+  }
+}
